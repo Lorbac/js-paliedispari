a5d380b7f2fa85aaacbffe0f32b5c76d3431fbad
Lorbac <baccini.lorenzo@gmail.com>
2021-07-23 18:05:25 +0200

    Esercizio "Pari Dispari" fixed

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -1,10 +1,22 @@
-var sceltaUtente = prompt("Scegli pari o dispari");
+// Chiedo all'utente di inserire per forza pari o dispari
+
+do {
+    var sceltaUtente = prompt("Scegli pari o dispari");
+}
+while (sceltaUtente != "pari" && sceltaUtente != "dispari");
 
 console.log("Hai scelto " + sceltaUtente);
 
-var numeroUtente = parseInt(prompt("Inserisci un numero"));
+// Chiedo all'utente di inserire per forza un numero da 1 a 5
+
+do {
+    var numeroUtente = parseInt(prompt("Inserisci un numero"));
+}
+while (isNaN(numeroUtente) || numeroUtente <= 1 || numeroUtente >= 5);
 
 console.log("Il tuo numero è " + numeroUtente);
+
+// Funzione per far generare un numero random al pc
 
 function rdnNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -13,6 +25,8 @@ function rdnNumber(min, max) {
 var numeroPc = rdnNumber(1,5);
 
 console.log("Il numero del pc è " + numeroPc);
+
+// Faccio la somma del mio numero e di quello del pc e in base alla scelta determino chi ha vinto
 
 var somma = numeroUtente + numeroPc;
 
